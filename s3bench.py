49c73d13c2a1d4af3f1897054eb26c09061ccfdb
Daisuke Miyamoto <midaisuk@gmail.com>
2018-11-20 11:23:53 +0000

    add benchmark recorder

--- s3bench.py
+++ s3bench.py
@@ -7,9 +7,58 @@ import random
 import string
 import time
 import multiprocessing
+import pandas as pd
+import requests
+import datetime
 
 
-class S3Benchmark():
+class BenchmarkRecorder:
+    def __init__(self, col_names, add_metadata=True):
+        metadata_col_names = ['instance_type', 'availability-zone', 'version', 'date', 'datetime']
+        self.add_metadata = add_metadata
+        if add_metadata:
+            col_names.extend(metadata_col_names)
+
+        self.df = pd.DataFrame(columns=col_names)
+        self.col_names = col_names
+
+    def _get_metadata(self, key):
+        response = requests.get('http://169.254.169.254/latest/meta-data/' + key)
+        return response.text
+        
+    def _get_metadata_record(self):
+        metadata = [
+            self._get_metadata('instance-type'),
+            self._get_metadata('placement/availability-zone'),
+            '2018-11-20',
+            str(datetime.date.today()),
+            str(datetime.datetime.now())
+        ]
+        return metadata
+        
+    def add_record(self, record):
+        if self.add_metadata:
+            record.extend(self._get_metadata_record())
+
+        s = pd.Series(record, index=self.col_names)
+        self.df = self.df.append(s, ignore_index=True)
+
+    def write(self, filename, file_type='csv'):
+        if file_type == 'csv':
+            self.df.to_csv(filename)
+        elif file_type == 'json':
+            self.df.to_json(filename)
+        else:
+            print('Error: wrong output type')
+
+    def upload_to_s3(self, tmp_filename, bucket_name, key_name, file_type='csv'):
+        self.write(tmp_filename, file_type)
+        s3 = boto3.client('s3')
+        s3.upload_file(tmp_filename, bucket_name, key_name)
+        print('Results uploaded to s3://%s/%s' % (bucket_name, key_name))
+
+
+class S3Benchmark:
     def __init__(self, bucket_name, max_concurrency=10, max_io_queue=100, random_data=True, clean=True):
         self.bucket_name = bucket_name
         self.local_tmp_file_list = []
@@ -67,6 +116,8 @@ class S3Benchmark():
         if self.clean:
             self._clean()
 
+        return upload_time, download_time
+
     def multi_run(self, num_threads, file_size_mb):
         print('Testing %d MB x %d Process:' % (file_size_mb, num_threads))
 
@@ -93,6 +144,8 @@ class S3Benchmark():
         if self.clean:
             self._clean()
 
+        return upload_time, download_time
+
     def _clean(self):
         s3 = boto3.client('s3')
         for tmp_file in self.local_tmp_file_list:
@@ -108,27 +161,59 @@ class S3Benchmark():
 if __name__=='__main__':
     s3_bucket_name = 'midaisuk-s3-test'
 
+    max_concurrency_list = [10, 100]
+    max_io_queue_list = [1000, 10000]
+    # filesize_list = [32, 64]
     filesize_list = [32, 64, 128, 256, 512, 1024, 2048]
-    threads_list = [1, 2, 4, 8]
-
-    max_concurrency = 100
-    max_io_queue = 1000
+    # threads_list = [1, 2]
+    threads_list = [1, 2, 4, 8, 16, 32]
+    trial = 3
     random_data = True
     
-    print('Settings:')
-    print(' * Max Concurrency: %d' % max_concurrency)
-    print(' * Max IO Queue: %d' % max_io_queue)
-    print(' * Random Data: %s' % random_data)
-    
-    s3bench = S3Benchmark(s3_bucket_name,
-        max_concurrency=max_concurrency,
-        max_io_queue=max_io_queue,
-        random_data=random_data
-    )
+    recorder = BenchmarkRecorder([
+        'trial'
+        'file_size',
+        'max_concurrency',
+        'max_io_queue',
+        'random_data',
+        'num_process',
+        'upload_time_sec',
+        'download_time_sec',
+        'upload_speed_mbps',
+        'download_speed_mbps',
+    ])
 
-    for filesize in filesize_list:
-        s3bench.run(filesize)
+    for max_concurrency in max_concurrency_list:
+        for max_io_queue in max_io_queue_list:
+            for num_process in threads_list:
+                for filesize in filesize_list:
+                    print('Settings:')
+                    print(' * Max Concurrency: %d' % max_concurrency)
+                    print(' * Max IO Queue: %d' % max_io_queue)
+                    print(' * Random Data: %s' % random_data)
 
-    for num_threads in threads_list:
-        for filesize in filesize_list:
-            s3bench.multi_run(num_threads, filesize)
+                    for i in range(trial):
+                        s3bench = S3Benchmark(s3_bucket_name,
+                            max_concurrency=max_concurrency,
+                            max_io_queue=max_io_queue,
+                            random_data=random_data
+                        )
+                        
+                        upload_time, download_time = s3bench.multi_run(num_process, filesize)
+                        recorder.add_record([
+                                trial,
+                                filesize,
+                                max_concurrency,
+                                max_io_queue,
+                                random_data,
+                                num_process,
+                                upload_time,
+                                download_time,
+                                filesize * num_process / upload_time * 8,
+                                filesize * num_process  / download_time * 8,
+                            ]
+                        )
+
+    print(recorder.df)
+    today = datetime.date.today()
+    recorder.upload_to_s3('result.tmp', 'midaisuk-benchmarks', 's3/throughput/' + str(today) + '.csv', file_type='csv')
